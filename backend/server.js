39fe6455eac79617f620f575569547c2fad27c43
Gouri <gouri.tshekar@gmail.com>
2026-05-26 00:46:15 +0530

    Fixed deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
+
 const mongoose = require("mongoose");
+
 const cors = require("cors");
 
 const bookingsRoute = require("./routes/bookings");
@@ -7,20 +9,28 @@ const bookingsRoute = require("./routes/bookings");
 const app = express();
 
 app.use(cors());
+
 app.use(express.json());
 
 mongoose.connect(
-  "mongodb+srv://gouritshekar:Gouri@cluster0.kiwsyfm.mongodb.net/?appName=Cluster0"
+  "mongodb+srv://gouritshekar:Gouri@cluster0.kiwsyfm.mongodb.net/hotelBooking?retryWrites=true&w=majority"
 )
+
 .then(() => {
+
   console.log("MongoDB Connected");
 })
+
 .catch((err) => {
+
   console.log(err);
 });
 
 app.use("/api/bookings", bookingsRoute);
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+
+  console.log(`Server running on port ${PORT}`);
 });
